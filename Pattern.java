f6aff8d717da766e2079379b92da8b3c2cd5db52
Arvind kumar <arvindkumar628736@gmail.com>
2026-08-09 23:21:03 +0530

    Add Pattern program

---
+++ Pattern.java
@@ -0,0 +1,25 @@
+public class Pattern{
+  public static void main(String[] args){
+    System.out.println("right half pyramid");
+    System.out.println("*");
+    System.out.println("* *");
+    System.out.println("* * *");
+    System.out.println("* * * *");
+    System.out.println("* * * * * *");
+
+    System.out.println("reverse right half pyramid");
+    System.out.println("* * * * * ");
+    System.out.println("* * * * ");
+    System.out.println("* * * ");
+    System.out.println("* * ");
+    System.out.println("* ");
+    System.out.println("left half pyramid");
+    System.out.println("         *");
+    System.out.println("       * *");
+    System.out.println("     * * *");
+    System.out.println("   * * * *");
+    System.out.println(" * * * * *");
+
+
+  }
+}
